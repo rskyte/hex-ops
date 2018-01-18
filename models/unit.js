c513d80c7ce5c7fc097052d6c58c03a51779a12e
James Lizamore <jwlizamore@gmail.com>
2018-01-18 17:53:55 +0000

    Added basic unit implementation

--- models/unit.js
+++ models/unit.js
@@ -1,4 +1,29 @@
 class Unit{
-  constructor(){}
+  constructor(name, vehicleAttack, infantryAttack, vehicleDefense, infantryDefense, unitType, movementRange){
+    this.name = name;
+    this.vehicleAttack = vehicleAttack;
+    this.infantryAttack = infantryAttack;
+    this.vehicleDefense = vehicleDefense;
+    this.infantryDefense = infantryDefense;
+    this.unitType = unitType;
+    this.movementRange = movementRange;
+  }
 
+  move(){}
+
+  attack(unit){
+    if(unit.unitType === "vehicle"){
+      return this.vehicleAttack;
+    } else {
+      return this.infantryAttack;
+    }
+  }
+
+  defendAgainst(unit){
+    if(unit.unitType === "vehicle"){
+      return this.vehicleDefense;
+    } else {
+      return this.infantryDefense;
+    }
+  }
 }
