09e4138bbe9b40433aa279c1b5cc0b7b95139677
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-18 20:14:28 +0000

    adds ability to have objects within test blocks

--- tut/tester.js
+++ tut/tester.js
@@ -16,7 +16,9 @@ dont = function (test) {
 
 Expect.prototype.toEqual = function (parameter) {
   this.message = `to equal ${parameter}`
+  console.log (this.parameter+ ': ' + parameter)
   this.result = (this.parameter === parameter)
+  console.log(this.result)
   return this
 }
 
@@ -54,7 +56,7 @@ function Test(name, lines){
 Test.prototype.run = function(){
   document.getElementById('tests').innerHTML += `<div id="tests" class="container-flex it-title"><h4>${this.name}</h4></div>`;
   this.lines.forEach(function(line){
-    if (typeof (line) === 'object'){ line.formatResult()}
+    if (line instanceof Expect){console.log(line); line.formatResult()}
   })
 }
 
