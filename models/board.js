37b4aab77da84bfa978ada36dfb0fb87b57c1b58
James Lizamore <jwlizamore@gmail.com>
2018-01-18 18:27:12 +0000

    Added determineWinner and rollDice methods

--- models/board.js
+++ models/board.js
@@ -1,4 +1,13 @@
 class Board{
-  constructor(){}
+  constructor(){
 
+  }
+
+  determineWinner(attacker, defender){
+    return this.rollDice(attacker.attack(defender)) > this.rollDice(defender.defendAgainst(attacker)) ? attacker : defender
+  }
+
+  rollDice(numDice){
+    return (Array(numDice).fill(0)).map(function(x){return Math.ceil(Math.random() * 6)}).sum()
+  }
 }
