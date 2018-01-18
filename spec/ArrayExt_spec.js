e320aea3d9176f293c4c931a156a07bb25d466f9
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-18 20:13:12 +0000

    adds second passing test

--- spec/ArrayExt_spec.js
+++ spec/ArrayExt_spec.js
@@ -3,3 +3,10 @@ describe("Array#sum",[
   	expect([1,2,3,4,5,7].sum()).toEqual(22)
   	])
   ])
+describe("Array#remove",[
+  it("removes the specified element from the array",[
+  	array = [1,2,3,"unwanted",4,5],
+  	array.remove("unwanted"),
+  	dont(expect(array).toContain(["unwanted"]))
+  	])
+  ])
