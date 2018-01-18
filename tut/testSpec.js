16dc7b03d7f423c5eaf004bdd14899349d474d6f
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-18 18:52:31 +0000

    Adds Tut test framework

---
+++ tut/testSpec.js
@@ -0,0 +1,12 @@
+describe("Tut Tester",[
+	it("passing tests",[
+		expect(1).toEqual(1),
+  		dont(expect(1).toEqual(2)),
+  		expect([4,1,2]).toContain(1)
+  	]),
+  	it("failing tests",[
+  		expect(1).toEqual(2),
+  		dont(expect(1).toEqual(1)),
+  		expect([1,2]).toContain(4)
+  	])
+]);
---
+++ tut/tester.js
@@ -0,0 +1,85 @@
+function Expect(parameter) {
+  this.parameter = parameter
+  this.result = false
+  this.message = ""
+}
+
+expect = function (parameter) {
+  return new Expect(parameter)
+}
+
+dont = function (test) {
+  test.result = (!test.result)
+  test.message = "not " + test.message
+  return test
+}
+
+Expect.prototype.toEqual = function (parameter) {
+  this.message = `to equal ${parameter}`
+  this.result = (this.parameter === parameter)
+  return this
+}
+
+Expect.prototype.toContain = function(parameter) {
+  this.message = `to contain ${parameter}`
+  this.result = this.parameter.includes(parameter)
+  return this
+}
+
+
+Expect.prototype.formatResult = function () {
+  if (!this.result) {
+    document.getElementById('tests').innerHTML +=
+      `<div class="test"><h5 style="color: red;">Expected ${this.parameter} ${this.message}</h5></div>`
+  } else {
+    document.getElementById('tests').innerHTML +=
+      `<div class="test"><h5 style="color: green;">Test passed</h5></div>`
+  }
+}
+
+describe = function (name, its) {
+  document.getElementById('tests').innerHTML += `<div class="testTitle"><h3>${name}</h3></div>`;
+  its.forEach(function (test) {
+    test.run()
+  })
+  // document.getElementById('tests').innerHTML += ;
+
+};
+
+function Test(name, lines){
+  this.name = name
+  this.lines = lines
+}
+
+Test.prototype.run = function(){
+  document.getElementById('tests').innerHTML += `<div id="tests" class="container-flex it-title"><h4>${this.name}</h4></div>`;
+  this.lines.forEach(function(line){
+    if (typeof (line) === 'object'){ line.formatResult()}
+  })
+}
+
+it = function (name, lines) {
+  return (new Test(name,lines))
+};
+
+runSpec = function(){
+  specName = document.getElementById('specname').value
+  try{
+  eval(loadSpec('/spec/'+ specName + '.js'))
+  } catch(error){
+  console.log(error.stack)
+  }
+}
+
+loadSpec = function(path) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      return this.responseText;
+    }
+  };
+  xhttp.open("GET", path, true);
+  xhttp.send();
+}
+
+
