563971fc9b3e7b3a4664b0087e4e605ad9d03be1
Lars <larsfinlay@googlemail.com>
2018-02-19 08:30:53 +0000

    tile tests complete

--- models/tile.js
+++ models/tile.js
@@ -25,7 +25,7 @@ class Tile {
   //returns a random unit defending this tile, battle logic will be within it's
   //own class or within the board class initially.
   defend(){
-  	return (this.units[Math.floor(Math.random * this.units.length)])
+  	return (this.units[Math.floor(Math.random() * this.units.length)])
   }
 
   _unitSizes(){
--- spec/tileSpec.js
+++ spec/tileSpec.js
@@ -22,21 +22,30 @@ describe("Tile", [
 
 describe("Tile#add", [
   it("can add a unit into the tile's unit array", [
-    fakeUnit = new FakeUnit(1),
-    testTile.add(fakeUnit),
-    expect(testTile.units.includes(fakeUnit)).toEqual(true)
+    fakeUnit1 = new FakeUnit(1),
+    testTile.add(fakeUnit1),
+    expect(testTile.units.includes(fakeUnit1)).toEqual(true)
   ]),
 
   it("cannot add a unit that is too large into the tile's unit array", [
-    fakeUnit = new FakeUnit(8),
-    testTile.add(fakeUnit),
-    expect(testTile.units.includes(fakeUnit)).toEqual(false)
+    fakeUnit2 = new FakeUnit(8),
+    testTile.add(fakeUnit2),
+    expect(testTile.units.includes(fakeUnit2)).toEqual(false)
   ])
 ])
 
 describe("Tile#spaceLeft", [
   it("returns the amount of used space", [
     expect(new Tile().spaceLeft()).toEqual(6)
+  ])
+])
+
+describe("Tile#remove", [
+  it("removes a unit from the tile's unit array", [
+    fakeUnit3 = new FakeUnit(1),
+    testTile.add(fakeUnit3),
+    testTile.remove(fakeUnit3),
+    expect(testTile.units.includes(fakeUnit3)).toEqual(false)
   ])
 ])
 
@@ -47,5 +56,21 @@ describe("Tile#canAccomodate", [
 
   it("returns false if the unit's size breaches the capacity", [
     expect(testTile.canAccomodate(new FakeUnit(10))).toEqual(false)
+  ])
+])
+
+describe("Tile#defend", [
+  it("returns a unit on the tile", [
+    expect(testTile.defend() instanceof FakeUnit).toEqual(true)
+  ]),
+
+  //Will cause difficulties later on with other functions calling on Math.random
+  it("Math.random should be called", [
+    mathRandomCalled = false,
+    Math.random = function() {
+      mathRandomCalled = true
+    },
+    testTile.defend(),
+    expect(mathRandomCalled).toEqual(true)
   ])
 ])
