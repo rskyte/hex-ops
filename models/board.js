74a07e686092db46c2a5b8d111a31cb79cbe77fa
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-02-20 17:54:45 +0000

    Adds generation of board, and a getter method for tiles
    This is based on a central 0,0 co-ord

--- models/board.js
+++ models/board.js
@@ -1,6 +1,27 @@
 class Board{
-  constructor(){
+  constructor(size = 4){
+  	this.size = size
+  	this.tiles = new Array(size*2 + 1).fill(new Array(size * 2 + 1))
+  }
 
+  generateTiles(){
+  	for(x = -this.size; x <= this.size; x++){
+  	  for(y = -this.size; y<= this.size; y++){
+  	    if(Math.abs(x+y) <= this.size){
+  	      _setTile(x,y, new Tile)
+  	    }
+  	  }
+  	}
+  }
+
+  _setTile(x,y,value){
+  	this.tiles[x+this.size][y+this.size]= value
+  }
+
+  tile(x,y){
+  	console.log(this.tiles)
+  	console.log(this.tiles[4][4])
+  	return this.tiles[x+this.size][y+this.size]
   }
 
   determineWinner(attacker, defender){
--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,5 +1,16 @@
 testBoard = new Board
 
+describe("Board#tile", [
+  it("should return an element in the tiles array, using co-ords centred on the board size",[
+    testBoard.tiles[4][4] = 5,
+    expect(testBoard.tile(0,0)).toEqual(5)
+  ]),
+])
+
+describe("Board#generateTiles", [
+  it("generates a set of tiles that map to a hexagonal design", [])
+])
+
 describe("Board#determineWinner", [
   it("should return attacker if defender was slain",[
     expect(testBoard.determineWinner(fakeAttacker = new FakeUnit(0,1),
