3156d7b57117b60e3076cfcbefe968d8c557aa0a
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-02-26 15:44:00 +0000

    Board generateTiles method tested and functioning

--- models/board.js
+++ models/board.js
@@ -1,26 +1,24 @@
 class Board{
   constructor(size = 4){
   	this.size = size
-  	this.tiles = new Array(size*2 + 1).fill(new Array(size * 2 + 1))
+  	this.tiles = new Array(size * 2 + 1).fill(0).map(function(){return new Array(size * 2 + 1)})
   }
 
   generateTiles(){
-  	for(x = -this.size; x <= this.size; x++){
-  	  for(y = -this.size; y<= this.size; y++){
+  	for(var x = -this.size; x <= this.size; x++){
+  	  for(var y = -this.size; y<= this.size; y++){
   	    if(Math.abs(x+y) <= this.size){
-  	      _setTile(x,y, new Tile)
+  	      this._setTile(x,y, new Tile)
   	    }
   	  }
   	}
   }
 
   _setTile(x,y,value){
-  	this.tiles[x+this.size][y+this.size]= value
+  	(this.tiles[x+this.size])[y+this.size] = value
   }
 
   tile(x,y){
-  	console.log(this.tiles)
-  	console.log(this.tiles[4][4])
   	return this.tiles[x+this.size][y+this.size]
   }
 
